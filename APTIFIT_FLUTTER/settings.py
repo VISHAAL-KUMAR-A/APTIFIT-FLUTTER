ecd19156681751b145698d2ef95fda974b34465a
VISHAAL-KUMAR-A <vkthedon123@gmail.com>
2025-04-23 10:04:49 +0530

    Added cors in the settings

--- APTIFIT_FLUTTER/settings.py
+++ APTIFIT_FLUTTER/settings.py
@@ -51,6 +51,7 @@ INSTALLED_APPS = [
     'rest_framework.authtoken',
     'dj_rest_auth',
     'dj_rest_auth.registration',
+    'corsheaders',
 ]
 SITE_ID = 1
 
@@ -63,7 +64,10 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'allauth.account.middleware.AccountMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
 ]
+
+CORS_ALLOW_ALL_ORIGINS = True
 
 ROOT_URLCONF = 'APTIFIT_FLUTTER.urls'
 
