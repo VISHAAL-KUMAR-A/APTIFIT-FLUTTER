26ddc5c9ac3f9745b45f56e47ef85e4fe0366b7c
VISHAAL-KUMAR-A <vkthedon123@gmail.com>
2025-05-26 21:26:36 +0530

    Fixing bugs

--- APTIFIT_FLUTTER/asgi.py
+++ APTIFIT_FLUTTER/asgi.py
@@ -7,11 +7,30 @@ For more information on this file, see
 https://docs.djangoproject.com/en/4.2/howto/deployment/asgi/
 """
 
-from APTIFIT_FLUTTER.routing_config import get_asgi_application
+from fitness import routing
+from channels.routing import ProtocolTypeRouter, URLRouter
+from channels.auth import AuthMiddlewareStack
 import os
 import django
+from django.core.asgi import get_asgi_application
 
+# Set Django settings module FIRST
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'APTIFIT_FLUTTER.settings')
+
+# Setup Django BEFORE importing any Django models or apps
 django.setup()
 
-application = get_asgi_application()
+# Now import Django ASGI application
+django_asgi_app = get_asgi_application()
+
+# Import Django Channels components AFTER django.setup()
+
+# Create the ASGI application
+application = ProtocolTypeRouter({
+    "http": django_asgi_app,
+    "websocket": AuthMiddlewareStack(
+        URLRouter(
+            routing.websocket_urlpatterns
+        )
+    ),
+})
